5f0b3c4974a4867fd307bed1e6ee1ac9d1b540d3
Misagh Moayyed <mmoayyed@unicon.net>
2017-04-11 15:04:56 +0430

    updated

--- app.py
+++ app.py
@@ -1,18 +1,37 @@
-from flask import Flask, render_template, session
+from flask import Flask, render_template, session, redirect
 from flask_cas import CAS
 from flask_cas import login
 from flask_cas import logout
 from flask_cas import login_required
+import logging
 
 app = Flask(__name__)
 cas = CAS(app, '/cas')
 app.config['CAS_SERVER'] = 'https://jasigcas.herokuapp.com' 
 app.config['CAS_AFTER_LOGIN'] = 'secure'
+# app.config['CAS_LOGOUT_ROUTE'] =
+# app.config['CAS_VALIDATE_ROUTE'] =
+# app.config['CAS_VALIDATE_ROUTE'] =
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return render_template('logout.html')
 
 @app.route("/secure")
 @login_required
 def secure():
-    return render_template('secure.html')
+    username = cas.username
+    attributes = cas.attributes
+
+    logging.info('CAS username: %s', username)
+    logging.info('CAS attributes: %s', attributes)
+
+    return render_template('secure.html', cas=cas)
+
+@app.route("/caslogout")
+def caslogout():
+    return redirect(app.config['CAS_LOGOUT_ROUTE'], code=302)
 
 @app.route("/")
 def main():
